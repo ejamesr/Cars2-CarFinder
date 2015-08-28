1150b42dbbf6f49a906f61543ebfcaf1aabcf771
ejamesr <ejames.ruff@gmail.com>
2015-08-28 17:35:37 -0400

    Added link to original context page (source of each image)

--- Cars2/NgApps/Filters/NgFilters.js
+++ Cars2/NgApps/Filters/NgFilters.js
@@ -11,10 +11,20 @@ app.filter('fixAmp', function () {
         //    var str = input.substring(i, 5);
         //    var x = input.charCodeAt(i);
         //}
-        var str = input.replace('&amp;', '&');
+        //var str = input.replace('&amp;', '&');
+        //var str = str.replace('&quot;', '"');
+
+        var regexAmp = new RegExp('&amp;', 'g');
+        var regexQuote = new RegExp('&quot;', 'g');
+        var str = input.replace(regexAmp, '&')
+                .replace(regexQuote, '"');
+
+        // This next string messes up regex process, so must do manually
         var ae = String.fromCharCode(226) + String.fromCharCode(8364) + String.fromCharCode(8220);
-        str = str.replace(ae, '--');
-        str = str.replace(ae, '--');
+        do {
+            var len = str.length;
+            str = str.replace(ae, '--');
+        } while (str.length != len);
         return str;
     };
 });
